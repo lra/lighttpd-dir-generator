9accb3e73431d057975e5aeb494946407ab0bbe9
Laurent Raufaste <analogue@glop.org>
2010-05-18 17:28:55 +0200

    Switched doctype to HTML5

--- dir-generator.php
+++ dir-generator.php
@@ -152,28 +152,29 @@ function get_file_type($file) {
 
 // Print the heading stuff
 $vpath = ($path != "./")?$path:"";
-print "<?xml version='1.0' encoding='utf-8'?>
-<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.1//EN' 'http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd'>
-<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en'>
+print '<!DOCTYPE html>
+<html lang="en">
 	<head>
-		<title>Index of /" .$vpath. "</title>
-		<style type='text/css'>
+		<meta charset="utf-8">
+		<title>Index of /'.$vpath.'</title>
+		<style type="text/css">
 		a, a:active {text-decoration: none; color: blue;}
 		a:visited {color: #48468F;}
 		a:hover, a:focus {text-decoration: underline; color: red;}
 		body {background-color: #F5F5F5;}
 		h2 {margin-bottom: 12px;}
 		table {margin-left: 12px;}
-		th, td { font-family: 'Courier New', Courier, monospace; font-size: 10pt; text-align: left;}
+		th, td { font-family: "Courier New", Courier, monospace; font-size: 10pt; text-align: left;}
 		th { font-weight: bold; padding-right: 14px; padding-bottom: 3px;}
 		td {padding-right: 14px;}
 		td.s, th.s {text-align: right;}
 		div.list { background-color: white; border-top: 1px solid #646464; border-bottom: 1px solid #646464; padding-top: 10px; padding-bottom: 14px;}
-		div.foot, div.script_title { font-family: 'Courier New', Courier, monospace; font-size: 10pt; color: #787878; padding-top: 4px;}
+		div.foot, div.script_title { font-family: "Courier New", Courier, monospace; font-size: 10pt; color: #787878; padding-top: 4px;}
 		div.script_title {float:right;text-align:right;font-size:8pt;color:#999;}
 		</style>
 	</head>
-	<body>";
+	<body>
+';
 
 if ($display_header)
 {
