8ef25439ab4e8d5602ffeee99950743eeba01d21
Laurent Raufaste <analogue@glop.org>
2010-05-18 16:56:47 +0200

    Warning fixed

--- dir-generator.php
+++ dir-generator.php
@@ -114,7 +114,7 @@ function format_bytes($size, $precision=0) {
 // This function returns the mime type of $file.
 //
 function get_file_type($file) {
-	global $image_types, $movie_types;
+	global $image_types, $movie_types, $archive_types, $document_types, $font_types;
 
 	$pos = strrpos($file, ".");
 	if ($pos === false) {
